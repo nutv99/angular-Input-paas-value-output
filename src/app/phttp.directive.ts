af1e3782c2708816520bf57923a23476a0f59433
nutv99 <nutv99@gmail.com>
2022-11-05 08:35:38 +0700

    2 files modified

--- src/app/phttp.directive.ts
+++ src/app/phttp.directive.ts
@@ -11,7 +11,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   selector: '[appPhttp]',
 })
 export class PhttpDirective {
-  // @Output() appHttpOut = new EventEmitter();
+  @Input() apiPath99: string = '';
   @Output() appHttpOut: EventEmitter<any> = new EventEmitter();
   myurl = 'th/category/All/1';
   apiPathInput = 'th/category/All/1';
@@ -20,7 +20,7 @@ export class PhttpDirective {
   constructor(private http: HttpClient) {}
   @HostListener('input', ['$event']) LoadDataFromBackEnd(event: any) {
     this.myurl =
-      'https://lovetoshopmall.com/swagger/marlinshopWork2/' + this.apiPathInput;
+      'https://lovetoshopmall.com/swagger/marlinshopWork2/' + this.apiPath99;
 
     this.http.get<any>(this.myurl).subscribe((data) => {
       // อ่านค่า result จาก JSON response ที่ส่งออกมา
